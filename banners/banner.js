e37a4d6917685b8635b2d4c2fc334d626176d886
adhamsteve213 <adhamsteve21@gmail.com>
2025-12-04 00:15:35 +0200

    Add files via upload

---
+++ banners/banner.js
@@ -0,0 +1,12 @@
+import banner1 from './banner 1 pizza.webp';
+import banner2 from './Screenshot 2025-10-28 104404.png';
+import banner3 from './Screenshot 2025-10-28 104423.png';
+import banner4 from './Screenshot 2025-10-28 104433.png';
+
+export const banners = [
+   { img: banner1 },
+    { img: banner2 },
+    { img: banner3 },
+    { img: banner4 },
+];
+export default banners;
